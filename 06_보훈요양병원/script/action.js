668157ac11bb7086662150cc3bdbf9859f99d1a6
Jiwoo Jeon <djs2009@naver.com>
2022-04-23 00:19:11 +0900

    보훈요양병원

--- 06_보훈요양병원/script/action.js
+++ 06_보훈요양병원/script/action.js
@@ -58,18 +58,66 @@ $(document).ready(function(){
         $(this).find('span').text('0'+(article4LiIndex+1))
     })
 
+    if($('section2').length >= 1){
+        var sec2Top = $('section2').offset()
+    }
 
-    
+    var sec2Top = $('.section2').offset().top();
 
     $(window).scroll(function(){
         var scrT = $(window).scrollTop();
+        var winH = $(window).height();
 
         if(scrT > 90){
             $('#header').addClass('action');
+            $('#visual_main').addClass('action');
         } else {
             $('#header').removeClass('action');
+            $('#visual_main').removeClass('action');
         }
 
+
+        if (scrT > sec2Top-winH/1.5){
+            $('.section2 .article2').addClass('action')
+        } else {
+            $('.section2 .article2').removeClass('action')
+        }
+
+    });
+
+    //snb 현재 페이지 표시
+    var pageUrl = window.location.href;
+
+    console.log('현재페이지주소는 : '+pageUrl)
+
+
+    $('.gnb .lnb a').each(function(){
+        var gnbHref = $(this).attr('href');
+
+        if(pageUrl.indexOf(gnbHref) >= 0){
+            var visualText = $(this).parents('.lnb').siblings('a').text();
+            $('.visual_text p').text(visualText);
+
+            var lnbHtml = $(this).parents('.lnb').html();
+            $('.snb').html(lnbHtml);
+        }
     })
+
+
+    $('.snb li a').each(function(){
+        var snbHref = $(this).attr('href');
+        // console.log(pageUrl.indexOf('snb'))
+
+        if(pageUrl.indexOf(snbHref) >= 0){
+            $(this).parent().addClass('active');
+        }
+    })
+
+
+    //제목 불러오기
+    var activeText = $('.snb li.active').text();
+    // alert(activeText)
+    $('.content_container h2').text(activeText)
+    
     
 })
